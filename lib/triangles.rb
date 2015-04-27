f051f5b472eddabe8b9ce1473eba311eacee1bac
ian bevel & Jim Patt <student@epicodus.com>
2015-04-27 16:53:58 -0700

    Adds logic to deny impossible triangles

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -1,17 +1,29 @@
 class Triangles
   define_method(:initialize) do |side1, side2, side3|
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
+    @side1 = side1.to_i
+    @side2 = side2.to_i
+    @side3 = side3.to_i
+  end
+
+  define_method(:triangle?) do
+    if @side1 + @side2 > @side3 && @side1 + @side3 > @side2 && @side2 + @side3 > @side1
+      true
+    else
+      false
+    end
   end
 
   define_method(:triangle_type) do
-    if @side1 != @side2 && @side1 != @side3 && @side2 != @side3
-      return "scaline"
-    elsif @side1 == @side2 && @side1 == @side3 && @side2 == @side3
-      return "equilateral"
+    if self.triangle?()
+      if @side1 != @side2 && @side1 != @side3 && @side2 != @side3
+        return "scaline"
+      elsif @side1 == @side2 && @side1 == @side3 && @side2 == @side3
+        return "equilateral"
+      else
+        return "isosceles"
+      end
     else
-      return "isosceles"
+      "this is not a triangle dummy"
     end
   end
 end
--- spec/triangles_spec.rb
+++ spec/triangles_spec.rb
@@ -5,7 +5,7 @@ require('pry')
 describe(:Triangles) do
   describe("#triangle_type") do
     it("returns scaline for a triangle with zero equal sides") do
-      test_triangle = Triangles.new("1","2","3")
+      test_triangle = Triangles.new("3","4","5")
       expect(test_triangle.triangle_type()).to(eq("scaline"))
     end
 
@@ -14,8 +14,18 @@ describe(:Triangles) do
       expect(test_triangle.triangle_type()).to(eq("equilateral"))
     end
     it("returns isosceles for a triangle with only 2 equal sides") do
-      test_triangle = Triangles.new("1", "1", "2")
+      test_triangle = Triangles.new("2", "2", "3")
       expect(test_triangle.triangle_type()).to(eq("isosceles"))
+    end
+
+    it("returns false if provided lenths make an impossible triangle") do
+      test_triangle = Triangles.new("2", "4", "9")
+      expect(test_triangle.triangle?()).to(eq(false))
+    end
+    
+    it("returns this is not a triangle if it is not a triangle") do
+      test_triangle = Triangles.new("2", "4", "9")
+      expect(test_triangle.triangle_type()).to(eq("this is not a triangle dummy"))
     end
   end
 end
