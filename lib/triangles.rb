c8eb7b04552249cc5ba4cf97e806a5df99f619c3
ian bevel & Jim Patt <student@epicodus.com>
2015-04-27 11:58:47 -0700

    pass second spec

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -8,6 +8,8 @@ class Triangles
   define_method(:triangle_type) do
     if @side1 != @side2 && @side1 != @side3 && @side2 != @side3
       return "scaline"
+    elsif @side1 == @side2 && @side1 == @side3 && @side2 == @side3
+      return "equilateral"
     end
   end
 end
