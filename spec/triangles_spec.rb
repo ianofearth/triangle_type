c5e9d71ce594e4f95e9e4dc9ee44ee8a3ed7528b
ian bevel & Jim Patt <student@epicodus.com>
2015-04-27 11:55:45 -0700

    wrote second spec

--- spec/triangles_spec.rb
+++ spec/triangles_spec.rb
@@ -8,5 +8,10 @@ describe(:Triangles) do
       test_triangle = Triangles.new("1","2","3")
       expect(test_triangle.triangle_type()).to(eq("scaline"))
     end
+
+    it("equilateral for a triangle with 3 equal sides") do
+      test_triangle = Triangles.new("1","1","1")
+      expect(test_triangle.triangle_type()).to(eq("equilateral"))
+    end
   end
 end
